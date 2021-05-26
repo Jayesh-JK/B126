b9f7dc79b36166311622078a572785648bc3b63a
jayesh <jayesh@dell>
2021-05-26 10:48:15 -0700

    First commit

---
+++ Day_10_SpringBoot_Angular_Git_Project/src/main/java/com/cjc/main/Day10SpringBootAngularGitProjectApplication.java
@@ -0,0 +1,14 @@
+package com.cjc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day10SpringBootAngularGitProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("First Git Repository");
+		SpringApplication.run(Day10SpringBootAngularGitProjectApplication.class, args);
+	}
+
+}
